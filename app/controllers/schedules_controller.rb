b1d7b89da672932c3e9e0f3e811061825a39a544
Sean Thickson <sean.thickson@aceproject.space>
2018-11-22 11:45:45 -0600

    Added Checkout page / routes / schedules controller

---
+++ app/controllers/schedules_controller.rb
@@ -0,0 +1,6 @@
+class SchedulesController < ApplicationController
+
+  def create_order
+    # redirect_to '/species'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'species/show'
   get 'hunters/index'
   get 'hunters/show'
+  get 'schedules/create_order'
+  post 'schedules/create_order'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
