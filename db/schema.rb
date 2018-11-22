0ed0dc4626efa92c7ebfbd848c777e08d3dc4e4a
Sean Thickson <sean.thickson@aceproject.space>
2018-11-22 12:24:07 -0600

    Added province model / migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_143616) do
+ActiveRecord::Schema.define(version: 2018_11_22_182233) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -69,6 +69,11 @@ ActiveRecord::Schema.define(version: 2018_11_15_143616) do
     t.string "title"
     t.text "content"
     t.string "permalink"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
